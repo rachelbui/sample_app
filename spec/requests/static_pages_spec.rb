5f437af75598eab21e0dbe98ecd577ea37392284
Rachel Bui <hello@rachelbui.com>
2013-09-15 23:09:21 +1000

    Change to POSTGRESQL

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "Static pages" do
+let(:page_title) {"Ruby on Rails Tutorial Sample App "}
+
   describe "Home Page" do
   	it "should have the content 'Sample App' " do
   		visit '/static_pages/home'
   		expect(page).to have_content('Sample App')
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  		expect(page).to have_title(page_title+"| Home")
   	end
   end
 
@@ -13,7 +15,7 @@ describe "Static pages" do
   	it "should have the content 'Help' " do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  		expect(page).to have_title(page_title+"| Help")
   	end
   end
 
@@ -21,10 +23,16 @@ describe "Static pages" do
   	it "should have the content 'About Us' " do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  		expect(page).to have_title(page_title+"| About Us")
   	end
   end
 
-
+  describe "Contact page" do
+  	it "should have the title 'Contact' " do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact')
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+  	end
+  end
 end
 
